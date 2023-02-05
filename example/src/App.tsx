c8915129cd6e58bc61247ae45ac79386fb0b5c8b
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-02-05 15:34:35 +0100

    fix: rework classes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Button, EmitterSubscription, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import { PowerMeters } from 'react-native-cycling-sensors'
+import { PowerMeters, BleSensors, PowerMeter, CadenceMeter } from 'react-native-cycling-sensors'
 
 const App = () => {
   const [isScanning, setIsScanning] = useState(false); 
@@ -23,66 +23,60 @@ const App = () => {
     console.log(peripheral);
   }
 
-  const handleStopScan = () => {
-    console.log('Scan is stopped');
-    setIsScanning(false);
+  const handlePowerData = (data: any) => {
+    console.log('Power: ', data)
   }
 
-  const handleUpdateValueForCharacteristic = (data: any) => {
-    let charType = bleDevices.getCharacteristicType(data.characteristic)
-    console.log(charType)
-    switch (charType) {
-      case 'CyclingPowerMeasurement':
-        let dataArray = new Uint8Array(data.value)
-        let powerData = bleDevices.parseCyclingPowerMeasurement(dataArray)
-        console.log(powerData)
-        break;
-      case 'CyclingPowerVector':
-        console.log(data.value)
-        break;
-      default:
-        console.log('Unknown data')
-        break;
-    }
+  const handleCscData = (data: any) => {
+    console.log('CSC: ', data)
   }
+
+
+  // const handleStopScan = () => {
+  //   console.log('Scan is stopped');
+  //   setIsScanning(false);
+  // }
+
+  // const handleUpdateValueForCharacteristic = (data: any) => {
+  //   let charType = bleDevices.getCharacteristicType(data.characteristic)
+  //   console.log(charType)
+  //   switch (charType) {
+  //     case 'CyclingPowerMeasurement':
+  //       let dataArray = new Uint8Array(data.value)
+  //       let powerData = bleDevices.parseCyclingPowerMeasurement(dataArray)
+  //       console.log(powerData)
+  //       break;
+  //     case 'CyclingPowerVector':
+  //       console.log(data.value)
+  //       break;
+  //     default:
+  //       console.log('Unknown data')
+  //       break;
+  //   }
+  // }
 
   useEffect(() => {
     let subscriptionStopScan: EmitterSubscription
     let subscriptionDiscover: EmitterSubscription
-    let subscriptionUpdateValueForChar: EmitterSubscription
 
     const startBleSensors = async () => {
-      await bleDevices.requestPermissions()
-      await bleDevices.start()
-      subscriptionUpdateValueForChar = bleDevices.addListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic );
-      subscriptionStopScan = bleDevices.addListener('BleManagerStopScan', handleStopScan);
-      subscriptionDiscover = bleDevices.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral)
-      
-      await bleDevices.scan()
+      const bleSensor = new BleSensors()
+      await bleSensor.requestPermissions()
+      await bleSensor.start()
+      await bleSensor.startSensorDiscovery()
+      await bleSensor.subscribeToDiscovery(handleDiscoverPeripheral)
       await sleep(10000)
-      let discoveredDevices = await bleDevices.getDiscoveredPeripherals();
-      if (discoveredDevices.length >= 1){
-        console.log('First device found: ', discoveredDevices[0])
-        try {
-          await bleDevices.connect(discoveredDevices[0].id)
-          await bleDevices.startInstantPowerNotification(discoveredDevices[0].id)
-          await bleDevices.startPowerVectorNotification(discoveredDevices[0].id)
-        } catch(err) {
-          console.log('error starting notif')
-        }       
-        await sleep(5000)
-        try {
-          await bleDevices.stopInstantPowerNotification(discoveredDevices[0].id)
-          await bleDevices.stopPowerVectorNotification(discoveredDevices[0].id)
-        } catch(err) {
-          console.log('error starting notif')
-        }
-        
-        await sleep(3000)
-        await bleDevices.disconnectAll()
-      }
-      
-
+      const sensorList = await bleSensor.getDiscoveredSensors()
+      console.log(sensorList)
+      const pm = new PowerMeter(sensorList.CyclingPower[0]?.id)
+      const csc = new CadenceMeter(sensorList.CyclingPower[0]?.id)
+      await pm.connect()
+      await csc.connect()
+      await pm.subscribe(handlePowerData)
+      await csc.subscribe(handleCscData)
+      await sleep(5000)
+      await pm.disconnect()
+      await csc.disconnect()
     };
   
     startBleSensors(); // run it, run it
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { BleCycling } from "./bleCycling";
 import PowerMeters from "./powerMeter";
+import { BleSensors, PowerMeter, CadenceMeter } from "./bleSensors";
 
-export { BleCycling, PowerMeters }
+export { BleCycling, PowerMeters, BleSensors, PowerMeter, CadenceMeter }
